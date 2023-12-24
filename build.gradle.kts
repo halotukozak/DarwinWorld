fe4d38c03773d1b331581c9a7a007641212c73b7
bartlomiejkozak <bartlomiejkozak@proton.me>
2023-12-24 13:24:59 +0100

    ad kotest with sample test

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "1.9.21"
+  kotlin("jvm") version "1.9.21"
 //    id("org.openjfx.javafxplugin") version "0.1.0"
 }
 
@@ -7,23 +7,28 @@ group = "org.oolab"
 version = "1.0-SNAPSHOT"
 
 repositories {
-    mavenCentral()
+  mavenCentral()
 }
 
 //javafx {
 //    modules("javafx.controls", "javafx.fxml")
 //}
 
+val kotestVersion = "5.8.0"
+
 dependencies {
 //    implementation ("no.tornado:tornadofx")
+//  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0-RC2")
 
-    testImplementation("org.jetbrains.kotlin:kotlin-test")
+  testImplementation("org.jetbrains.kotlin:kotlin-test")
+  testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
+  testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
 }
 
 tasks.test {
-    useJUnitPlatform()
+  useJUnitPlatform()
 }
 
 kotlin {
-    jvmToolchain(21)
+  jvmToolchain(21)
 }
---
+++ src/test/kotlin/GenomeTest.kt
@@ -0,0 +1,23 @@
+import Gen.*
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.collections.shouldBeIn
+import io.kotest.matchers.shouldBe
+
+class GenomeTest : StringSpec({
+  "Genome should be infinite" {
+    listOf(
+      listOf(DmNotch, MDM2, zCycD1, Frp, NAC, sdf),
+      listOf(SHH, DmNotch, MDM2, zCycD1, Frp, NAC, sdf),
+      listOf(SHH, DmNotch, MDM2, zCycD1, Frp, NAC, sdf, SHH, DmNotch, MDM2, zCycD1, Frp, NAC, sdf),
+      listOf(Frp),
+      listOf(NAC, sdf),
+    )
+      .forEach { gens ->
+        val genome = Genome(gens)
+        (0..100).forEach {
+          genome.hasNext() shouldBe true
+          genome.next() shouldBeIn gens
+        }
+      }
+  }
+})
