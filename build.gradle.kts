4ecbea7ae20161c7d309a0d04e2dc944b8473bee
bartlomiejkozak <bartlomiejkozak@proton.me>
2023-12-28 17:47:25 +0100

    add required jvm args

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,14 @@ plugins {
 
 application {
   mainClass = "frontend.DarwinWorldApp"
+  applicationDefaultJvmArgs = listOf(
+    "--add-exports=javafx.controls/com.sun.javafx.scene.control.behavior=ALL-UNNAMED",
+    "--add-exports=javafx.controls/com.sun.javafx.scene.control=ALL-UNNAMED",
+    "--add-exports=javafx.base/com.sun.javafx.binding=ALL-UNNAMED",
+    "--add-exports=javafx.graphics/com.sun.javafx.stage=ALL-UNNAMED",
+    "--add-exports=javafx.controls/com.sun.javafx.scene.control.behavior=ALL-UNNAMED",
+    "--add-opens=javafx.graphics/javafx.scene=ALL-UNNAMED"
+  )
 }
 
 group = "org.oolab"
