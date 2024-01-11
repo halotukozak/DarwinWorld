75b39dd9cd0e63c4af42b8b97801a8acba0c1c91
bartlomiejkozak <bartlomiejkozak@proton.me>
2024-01-11 19:08:59 +0100

    use List<Pair<*, *>> instead of Map, use Flow

--- src/main/kotlin/backend/map/AbstractMap.kt
+++ src/main/kotlin/backend/map/AbstractMap.kt
@@ -5,9 +5,11 @@ import backend.Direction
 import backend.GenMutator
 import backend.Genome
 import backend.config.Config
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.flow.*
+import shared.group
+import shared.mapMax
+import shared.mapValues
+import shared.mapValuesAsync
 import kotlin.random.Random
 
 abstract class AbstractMap(protected val config: Config) {
@@ -18,10 +20,10 @@ abstract class AbstractMap(protected val config: Config) {
     (0..<config.mapHeight).map { y -> Vector(x, y) }
   }
 
-  protected val _animals = MutableStateFlow(fields.associateWith { emptyList<Animal>() })
+  protected val _animals = MutableStateFlow(fields.map { it to emptyList<Animal>() })
   protected val _plants = MutableStateFlow(emptySet<Vector>())
 
-  val animals: StateFlow<Map<Vector, List<Animal>>> = _animals
+  val animals: StateFlow<List<Pair<Vector, List<Animal>>>> = _animals
   val plants: StateFlow<Set<Vector>> = _plants
 
   init {
@@ -31,31 +33,36 @@ abstract class AbstractMap(protected val config: Config) {
       }
         .distinct()
         .take(config.initialAnimals)
-        .groupingBy { it }
-        .eachCount()
-        .mapValues { (_, n) ->
-          List(n) { Animal(config.initialAnimalEnergy, Genome.random(config.genomeLength), Direction.random()) }
+        .groupBy { it }
+        .toList()
+        .mapValues { vectors -> vectors.size }
+        .mapValues { n ->
+          List(n) {
+            Animal(
+              config.initialAnimalEnergy,
+              Genome.random(config.genomeLength),
+              Direction.random()
+            )
+          }
         }
     }
   }
 
-  private fun updateAnimals(f: Animal.() -> Animal) = _animals.update {
-    it.mapValues { (_, set) -> set.map(f) }
+  private suspend fun updateAnimals(f: Animal.() -> Animal) = _animals.update {
+    it.mapValuesAsync { set -> set.map(f) }
   }
 
-  fun growAnimals() = updateAnimals(Animal::grow)
-  fun rotateAnimals() = updateAnimals(Animal::rotate)
-  fun ageAnimals() = updateAnimals(Animal::age)
+  suspend fun growAnimals() = updateAnimals(Animal::grow)
+  suspend fun rotateAnimals() = updateAnimals(Animal::rotate)
+  suspend fun ageAnimals() = updateAnimals(Animal::age)
 
 
-  fun removeDeadAnimals() = _animals.update {
-    it.mapValues { (_, set) ->
-      set.filterNot(Animal::isDead)
-    }
+  suspend fun removeDeadAnimals() = _animals.update {
+    it.mapValuesAsync { set -> set.filterNot { it.isDead() } }
   }
 
-  fun moveAnimals() = _animals.update { animals ->
-    animals.flatMap { (position, set) ->
+  suspend fun moveAnimals() = _animals.update {
+    it.flatMap { (position, set) ->
       set.map { animal ->
         var newPosition = position + animal.direction.vector //todo (var)
         when {
@@ -69,13 +76,15 @@ abstract class AbstractMap(protected val config: Config) {
         newPosition to animal
       }
     }
-      .groupByTo(mutableMapOf(), { it.first }, { it.second })
+      .asFlow()
+      .group()
+      .toList()
   }
 
-  fun consumePlants() = _plants.update { plants ->
+  suspend fun consumePlants() = _plants.update { plants ->
     val p = plants.toMutableSet()
     _animals.update { animals ->
-      animals.mapValues { (position, set) ->
+      animals.mapValuesAsync { position, set ->
         set.mapMax {
           if (position in plants) {
             p.remove(position)
@@ -88,8 +97,8 @@ abstract class AbstractMap(protected val config: Config) {
   }
 
 
-  fun breedAnimals() = _animals.update { animals ->
-    animals.mapValues { (_, set) ->
+  suspend fun breedAnimals() = _animals.update { animals ->
+    animals.mapValuesAsync { set ->
       if (set.size >= 2) {
         val animal1 = set.max()
         val animal2 = (set - animal1).max()
@@ -106,10 +115,4 @@ abstract class AbstractMap(protected val config: Config) {
 
   abstract fun growPlants(plantsCount: Int)
 }
-
-private fun <T : Comparable<T>> Iterable<T>.mapMax(function: (T) -> T): List<T> =
-  maxOrNull()?.let { max -> map { if (it == max) function(it) else it } } ?: toList()
-
-private fun <R> (() -> R).invokeIf(predicate: () -> Boolean): R? = if (predicate()) invoke() else null
-
 
