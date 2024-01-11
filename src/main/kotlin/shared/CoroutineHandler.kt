0672f8be85138e65b5ad67f0862c6048d8e617d2
bartlomiejkozak <bartlomiejkozak@proton.me>
2024-01-11 19:08:14 +0100

    provide `CoroutineHandler` implementation

--- src/main/kotlin/shared/CoroutineHandler.kt
+++ src/main/kotlin/shared/CoroutineHandler.kt
@@ -2,43 +2,43 @@ package shared
 
 import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.javafx.JavaFx
 
 interface CoroutineHandler {
 
-  fun <T> Flow<T>.start(): Job
-
-  fun <T> Flow<T>.onUpdate(action: suspend (T) -> Unit) = onEach(action).start()
+  val coroutineScope: CoroutineScope
 
   fun launch(
     start: CoroutineStart = CoroutineStart.DEFAULT,
-    block: suspend CoroutineScope.() -> Unit
-  ): Job
+    block: suspend CoroutineScope.() -> Unit,
+  ): Job = coroutineScope.launch(start = start, block = block)
 
   fun launchDefault(
     start: CoroutineStart = CoroutineStart.DEFAULT,
-    block: suspend CoroutineScope.() -> Unit
-  ): Job
+    block: suspend CoroutineScope.() -> Unit,
+  ): Job = coroutineScope.launch(Dispatchers.Default, start, block)
 
   fun launchMain(
     start: CoroutineStart = CoroutineStart.DEFAULT,
-    block: suspend CoroutineScope.() -> Unit
-  ): Job
+    block: suspend CoroutineScope.() -> Unit,
+  ): Job = coroutineScope.launch(Dispatchers.JavaFx, start, block)
 
   fun launchMainImmediate(
     start: CoroutineStart = CoroutineStart.DEFAULT,
-    block: suspend CoroutineScope.() -> Unit
-  ): Job
+    block: suspend CoroutineScope.() -> Unit,
+  ): Job = coroutineScope.launch(Dispatchers.JavaFx.immediate, start, block)
 
   fun launchIO(
     start: CoroutineStart = CoroutineStart.DEFAULT,
-    block: suspend CoroutineScope.() -> Unit
-  ): Job
+    block: suspend CoroutineScope.() -> Unit,
+  ): Job = coroutineScope.launch(Dispatchers.IO, start, block)
 
   fun launchUnconfined(
     start: CoroutineStart = CoroutineStart.DEFAULT,
-    block: suspend CoroutineScope.() -> Unit
-  ): Job
+    block: suspend CoroutineScope.() -> Unit,
+  ): Job = coroutineScope.launch(Dispatchers.Unconfined, start, block)
 
   suspend fun <T> withDefault(block: suspend CoroutineScope.() -> T): T =
     withContext(Dispatchers.Default, block)
@@ -54,4 +54,8 @@ interface CoroutineHandler {
 
   suspend fun <T> withUnconfined(block: suspend CoroutineScope.() -> T): T =
     withContext(Dispatchers.Unconfined, block)
+
+  fun <T> Flow<T>.start() = launchIn(coroutineScope)
+
+  fun <T> Flow<T>.onUpdate(action: suspend (T) -> Unit) = onEach(action).start()
 }
