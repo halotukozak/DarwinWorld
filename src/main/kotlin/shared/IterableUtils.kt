6c469b97a7431c352dd9b65de6aa753711bfbc7b
bartlomiejkozak <bartlomiejkozak@proton.me>
2024-01-11 19:06:13 +0100

    introduce FlowUtils and IterableUtils

---
+++ src/main/kotlin/shared/IterableUtils.kt
@@ -0,0 +1,23 @@
+package shared
+
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.toList
+
+inline fun <T, U, V> List<Pair<T, U>>.mapValues(crossinline f: (T, U) -> V): List<Pair<T, V>> =
+  map { (t, u) -> t to f(t, u) }
+
+inline fun <T, U, V> List<Pair<T, U>>.mapValues(crossinline f: (U) -> V): List<Pair<T, V>> =
+  map { (t, u) -> t to f(u) }
+
+inline fun <T : Comparable<T>> Iterable<T>.mapMax(crossinline function: (T) -> T): List<T> =
+  maxOrNull()?.let { max -> map { if (it == max) function(it) else it } } ?: toList()
+
+private suspend inline fun <T> Iterable<T>.mapAsync(crossinline f: suspend (T) -> T): List<T> =
+  asFlow().map(f).toList()
+
+suspend inline fun <T, U, V> Iterable<Pair<T, U>>.mapValuesAsync(crossinline f: suspend (T, U) -> V): List<Pair<T, V>> =
+  asFlow().mapValues(f).toList()
+
+suspend inline fun <T, U, V> Iterable<Pair<T, U>>.mapValuesAsync(crossinline f: suspend (U) -> V): List<Pair<T, V>> =
+  asFlow().mapValues(f).toList()
