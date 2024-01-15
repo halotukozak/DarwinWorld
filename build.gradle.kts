6e2695eb4d06761989e144b12d9dc659505611e5
bartlomiejkozak <bartlomiejkozak@proton.me>
2024-01-15 22:44:43 +0100

    add atlanta stylesheet

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,6 +32,8 @@ val kotestVersion = "5.8.0"
 
 dependencies {
   implementation("no.tornado:tornadofx:1.7.20")
+  implementation("io.github.mkpaz:atlantafx-base:2.0.1")
+
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0-RC2")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-javafx:1.8.0-RC2")
 
--- src/main/kotlin/frontend/DarwinWorldApp.kt
+++ src/main/kotlin/frontend/DarwinWorldApp.kt
@@ -1,5 +1,13 @@
 package frontend
 
+import atlantafx.base.theme.PrimerLight
+import javafx.stage.Stage
 import tornadofx.*
 
-class DarwinWorldApp : App(ConfigView::class)
+
+class DarwinWorldApp : App(ConfigView::class){
+  override fun start(stage: Stage) {
+    setUserAgentStylesheet(PrimerLight().userAgentStylesheet)
+    super.start(stage)
+  }
+}
